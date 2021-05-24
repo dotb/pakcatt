541ea92413343bfc07ac9537e6b555d279e80aad
Bradley Clayton <bradley@squarepolka.com>
2021-05-24 22:08:26 +1000

    Proof-of-concept tested and working well

--- src/main/kotlin/pakcatt/configuration/Configuration.kt
+++ src/main/kotlin/pakcatt/configuration/Configuration.kt
@@ -11,6 +11,12 @@ class Configuration {
     @Value("\${pakcatt.service-name}")
     private lateinit var serviceName: String
 
+    @Value("\${pakcatt.serial-port-path}")
+    private lateinit var serialPortPath: String
+
+    @Value("\${pakcatt.serial-port-baud}")
+    private lateinit var serialPortBaud: Number
+
     @Bean
     fun restTemplate(): RestTemplate {
         return RestTemplate()
@@ -19,6 +25,16 @@ class Configuration {
     @Bean
     open fun serviceName(): String {
         return serviceName
+    }
+
+    @Bean
+    open fun serialPortPath(): String {
+        return serialPortPath
+    }
+
+    @Bean
+    open fun serialPortBaud(): Int {
+        return serialPortBaud.toInt()
     }
 
 }
